8404cd57987ab759da2b3bad45af1ba29d29c1b8
Paul Lynch <plynch@mail.nih.gov>
2020-01-24 17:31:59 -0500

    Added tests for the validation and conversion forms

--- demo-test/ucumDemo-spec.js
+++ demo-test/ucumDemo-spec.js
@@ -1,4 +1,6 @@
-// ucumDemo-spec.js
+// Protractor tests
+
+var EC = protractor.ExpectedConditions;
 describe('UCUM Demo page tests', function(){
   browser.waitForAngularEnabled(false);
 
@@ -18,6 +20,31 @@ describe('UCUM Demo page tests', function(){
     // verify that the validator tab page is initially active
     let vtab = element(by.id('validation-link'));
     expect(vtab.isDisplayed()).toBe(true);
+  });
+
+  describe('Validation', function() {
+    it('should validate a unit', function() {
+      browser.get('http://localhost:3003/demo.html');
+      browser.wait(EC.visibilityOf($("#valString")), 5000);
+      $('#valString').sendKeys('cm');
+      browser.wait(EC.visibilityOf($("#searchResults")), 5000);
+      $('#valString').sendKeys(protractor.Key.TAB);
+      browser.wait(EC.textToBePresentInElement($('#validationString'),
+        'cm (centimeter) is a valid unit expression.'), 5000);
+    });
+  });
+
+  describe('Conversion', function() {
+    it('should convert a unit', function() {
+      browser.get('http://localhost:3003/demo.html#converter');
+      browser.wait(EC.visibilityOf($("#convertFrom")), 5000);
+      $('#convertFrom').sendKeys('m');
+      $('#convertFrom').sendKeys(protractor.Key.TAB);
+      $('#convertTo').sendKeys('cm');
+      $('#convertTo').sendKeys(protractor.Key.TAB);
+      browser.wait(EC.textToBePresentInElementValue($('#convertToNum'),
+        '100'), 2000);
+    });
   });
 
 });
