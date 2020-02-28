d675c85eff3016ae97c21d763c0cb88fe973976a
yuriy.sedinkin <yuriy.sedinkin@billing.ru>
2020-02-28 18:35:56 -0500

    remove ucumDefs.json before generation

--- impexp/updateData.js
+++ impexp/updateData.js
@@ -2,17 +2,17 @@
  * This is the tool for generating ucumDefs.json. See the README for details.
  */
 
-import { UcumXmlDocument } from "../source/ucumXmlDocument.js";
-import { UcumDataUpdater } from "./ucumDataUpdater.js";
-
 const params = process.argv.slice(2);
 
-if (!params.includes('--skip-ucum-essence-xml')) {
-  const docObj = UcumXmlDocument.getInstance();
-  docObj.parseXml();
-}
+const fs = require('fs');
+fs.unlinkSync('../data/ucumDefs.json');
+
+const UcumXmlDocument = require('../source/ucumXmlDocument.js').UcumXmlDocument;
+const docObj = UcumXmlDocument.getInstance();
+docObj.parseXml();
 
 if (!params.includes('--skip-ucum-csv')) {
+  const UcumDataUpdater = require('./ucumDataUpdater.js').UcumDataUpdater;
   const upd = UcumDataUpdater.getInstance();
   upd.readFile('../data/ucum.csv');
 }
