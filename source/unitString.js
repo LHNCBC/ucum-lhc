5078816d73cf5b0dfdca45a750964f2490c063b6
xieh3 <hao.xie@nih.gov>
2024-03-15 13:07:24 -0400

    do not allow beginning '/' in parenthesis

--- source/unitString.js
+++ source/unitString.js
@@ -535,6 +535,11 @@ export class UnitString {
             uString.substring(openPos + 1, closePos - 1), origString);
           if (parseResp[0] === null)
             stopProcessing = true;
+          else if (uString[openPos + 1] === '/') {
+            // If the term inside the parenthesis starts with '/', fail the validation. See LF2854.
+            this.retMsg_.push("Unary operator '/' is only allowed at the beginning of the main term, not inside a parenthesis.");
+            stopProcessing = true;
+          }
           else {
             origString = parseResp[1];
             this.parensUnits_[pu++] = parseResp[0];
